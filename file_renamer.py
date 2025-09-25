0b962efd62b124ea520dece2209fa4dfb854aae4
Taylor-on-Git <teejay282@googlemail.com>
2025-09-25 14:09:57 +0100

    feat: file renamer dry-run preview

--- file_renamer.py
+++ file_renamer.py
@@ -1,1 +1,8 @@
-print("file renamer script placeholder")
+import os
+
+folder = "."  # current folder
+for filename in os.listdir(folder):
+    old_path = os.path.join(folder, filename)
+    new_name = "new_" + filename
+    print(f"Would rename: {filename} -> {new_name}")
+
